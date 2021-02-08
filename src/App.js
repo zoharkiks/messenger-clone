373e227fb461f29215405e6eb60121aede9a736b
Zohar <zoharkiks@gmail.com>
2021-02-08 16:50:15 +0530

    Added to Git

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Message from "./Message";
+import db from "./firebase";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
+import { IconButton } from "@material-ui/core";
+
+
+
+function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+
+
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    db.collection("messages").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
+  };
+
+  return (
+    <div className="app">
+      <img
+        src="https://facebookbrand.com/wp-content/uploads/2019/10/Messenger_Logo_Color_RGB.png?w=100&h=100"
+        alt="logo"
+      />
+      <h1>Messenger</h1>
+      
+      
+      <h2>Welcome {username}</h2>
+      
+
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input
+            className="app__input"
+            placeholder="Enter a message..."
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+
+          <IconButton
+            className="app__sendButton"
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            <SendIcon />
+          </IconButton>
+          
+
+          <IconButton >
+            <EmojiEmotionsIcon />
+            
+          </IconButton>
+
+
+        </FormControl>
+      </form>
+
+      <FlipMove>
+        {messages.map(({ id, message }) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default App;
